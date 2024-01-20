c825b8a786725d79ed4b4de330d43078b4ee68e8
Tuan Nguyen <lemonroasted@duck.com>
2024-01-19 19:47:58 -0800

    Adds room page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from home.views import index
+from home.views import index, room
 
 urlpatterns = [
     path("", index, name="home"),
+    path("<str:room_name>/", room, name="room"),
 ]
--- home/views.py
+++ home/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 async def index(request: HttpRequest):
     return render(request, "home/home.html")
+
+
+async def room(request: HttpRequest, room_name):
+    return render(request, "home/room.html", {"room_name": room_name})
